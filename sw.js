4caca5bc3b58210894f87cbb2967ff74f4e4b196
bilalresearch8 <bilalresearch8@gmail.com>
2026-09-06 23:29:30 -0400

    Eureka Atlas v15 — refresh application shell

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eureka-atlas-v14-shell';
+const CACHE = 'eureka-atlas-v15-shell';
 const SHELL = ['./','./index.html','./manifest.webmanifest','./assets/icons/eureka-atlas.svg','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
